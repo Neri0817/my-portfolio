b5b9d994258b1c61208227c21e7aa80cd28cd67b
Neri <91215462+Neri0817@users.noreply.github.com>
2023-04-28 18:39:57 +0300

    Update background

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
 
-      <main></main>
+      <Home />
 
       <Footer />
     </>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,28 @@
 export const Header = () => {
   return (
     <header>
-      <div></div>
+      <div className="header-logo-container"></div>
+      <div className="header-container">
+        <nav>
+          <ul>
+            <li>
+              <a href="/">home</a>
+            </li>
+            <li>
+              <a href="/about">about</a>
+            </li>
+            <li>
+              <a href="/skills">skills</a>
+            </li>
+            <li>
+              <a href="/projects">projects</a>
+            </li>
+            <li>
+              <a href="/contact">contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
