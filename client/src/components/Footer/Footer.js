804fae44a96bf4e13abf557f53deef69e644da61
Neri <91215462+Neri0817@users.noreply.github.com>
2023-05-01 20:53:49 +0300

    Add footer and footer social icons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,32 @@
 export const Footer = () => {
   return (
     <footer>
-      <div></div>
+      <div className="footer-container">
+        <div className="footer-container-social">
+          <ul className="footer-container-social-list">
+            <li>
+              <a href="/">
+                <i className="fa-brands fa-instagram instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="fa-brands fa-github github"></i>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="fa-brands fa-square-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="fa-brands fa-linkedin linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 };
