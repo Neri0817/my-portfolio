e17118a36164c8571fed8dfd089a9f9e2aed1b1b
Neri <91215462+Neri0817@users.noreply.github.com>
2023-05-02 00:00:57 +0300

    Add footer and style footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import logo from "../../images/neri-logo.png";
 export const Footer = () => {
   return (
     <footer>
@@ -5,27 +7,36 @@ export const Footer = () => {
         <div className="footer-container-social">
           <ul className="footer-container-social-list">
             <li>
-              <a href="/">
+              <a href="https://www.instagram.com/neri0817/">
                 <i className="fa-brands fa-instagram instagram"></i>
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="https://github.com/Neri0817">
                 <i className="fa-brands fa-github github"></i>
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="https://www.facebook.com/neri0817">
                 <i className="fa-brands fa-square-facebook"></i>
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="https://www.linkedin.com/in/neri0817/">
                 <i className="fa-brands fa-linkedin linkedin"></i>
               </a>
             </li>
           </ul>
         </div>
+        <div className="footer-container-logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+      </div>
+      <div className="footer-info-container">
+        <p>All rights reserved. &#174;</p>
+        <p>&#169; 2023 Designed and built by Neri Mehmedova</p>
       </div>
     </footer>
   );
