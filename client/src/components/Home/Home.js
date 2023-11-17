c061487e606e71ec8a6a538d2186bdf3c257ee93
Neri <91215462+Neri0817@users.noreply.github.com>
2023-11-17 20:05:40 +0200

    css changes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,10 +2,16 @@ import video from "../../images/background-video.mp4";
 export const Home = () => {
   return (
     <main className="home-main">
-      <div className="home-container">
+      <div className="home-background-container">
         <video autoPlay muted loop id="myVideo">
           <source src={video} type="video/mp4" />
         </video>
+      </div>
+      <div className="home-container">
+        <div className="home-container-context"></div>
+        <div className="home-container-profile">
+          <div className="home-container-profile-img"></div>
+        </div>
       </div>
     </main>
   );
